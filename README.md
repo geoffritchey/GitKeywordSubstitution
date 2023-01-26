079269b8677843e0f69c55f89fc8033256820ed5
Geoffrey Ritchey <geoffrey.ritchey@gmail.com>
2023-01-26 15:15:21 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+Use git commands to replace variables from the standard input stream to the standard output stream.
+
+eg
+
+$Repo$ -> $Repo: https://github.com/geoffitchey/GitKeywordSubstitution.git$
+
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import git
 
 if __name__ == '__main__':
     repo = git.Repo(search_parent_directories=True)
-    sha = repo.head.commit.hexsha
-    print(repo.git.rev_parse(sha))
+    git_root = repo.git.rev_parse("--show-toplevel")
+    print(repo.remote().urls())
+    print(git_root)
+    name = git_root.split('/')[-1]
+    print(name)
     #for line in sys.stdin:
         #print(line.replace("$Repo%", Repo.rev_parse()))
-
-
